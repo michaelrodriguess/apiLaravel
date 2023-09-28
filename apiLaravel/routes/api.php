fe4bae8de03e09fd67bece182a249428bbba2337
Michael Rodrigues <run.michael97@gmail.com>
2023-09-28 00:00:51 -0300

    feat: Controllers and Route working

--- apiLaravel/routes/api.php
+++ apiLaravel/routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//api/version1
+Route::group(['prefix' => 'version1', 'namespace' => 'App\Http\Controllers\Api\version1'], function() {
+    Route::apiResource('customers', CustomerController::class);
+    Route::apiResource('invoices', InvoiceController::class);
+});
